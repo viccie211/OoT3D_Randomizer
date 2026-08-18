dec4f9c869d8c68c78f6082dd4c61ac7a672d96b
viccie211 <victorremmerswaal@hotmail.com>
2026-08-18 17:43:51 +0200

    Changed it to the grayscale mode

--- code/src/actors/frog.c
+++ code/src/actors/frog.c
@@ -25,9 +25,33 @@ void FrogSongTimer_Init(void) {
         sTimerFrogSong[i] = FrogSongTimerMultiplier(sTimerFrogSong[i]);
     }
 }
+#define GET_CMB_STATUS(cmbMan) (((CMB_HEAD*)cmbMan->cmbChunk)->name[0])
+#define CMBSTATUS_MODIFIED '-'
+#define CMBSTATUS_RESTORED '^'
+
+
+void EnFr_ButterflyColorOverrive(CmbManager* cmbMan, s16 objId, s32 cmbIdx) {
+    if (GET_CMB_STATUS(cmbMan) == CMBSTATUS_MODIFIED) {
+        return;
+    }
+
+    GET_CMB_STATUS(cmbMan) = CMBSTATUS_MODIFIED;
+    CMB_MATS* cmbMats      = Cmb_GetMatsChunk(cmbMan->cmbChunk);
+    CMB_TEX* cmbTex        = Cmb_GetTexChunk(cmbMan->cmbChunk);
+    // Modify all combiners to use operand `green` instead of `color`, making the texture draw in grayscale.
+    for (Combiner* combiner = Cmb_GetCombiners(cmbMats); (u32)combiner < (u32)cmbTex; combiner++) {
+        for (s32 opIdx = 0; opIdx < ARRAY_SIZE(combiner->operandColors); opIdx++) {
+            if (combiner->operandColors[opIdx] == COMBINEROP_COLOR) {
+                combiner->operandColors[opIdx] = COMBINEROP_GREEN;
+            }
+        }
+    }
+
+};
 
 void EnFr_rDraw(Actor* thisx, GlobalContext* ctx){
     CitraPrint("EnFr_rDraw Actor: %x Params: %x",thisx->id,thisx->params);
     ObjectEntry* obj = Object_FindEntry(OBJECT_FROG);
-    SoullessModels_ModifyGenericCmb(obj->zarInfo.cmbMans[1]);
+    EnFr_ButterflyColorOverrive(obj->zarInfo.cmbMans[1],-1,-1);
 }
+
